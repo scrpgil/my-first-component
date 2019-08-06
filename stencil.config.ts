8b7d64d2fc46794a0b7ca2fab7e926a1bbffc5ca
scrpgil <scraplbuild@gmail.com>
2019-08-06 13:45:08 +0900

    first commit

---
+++ stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'my-first-component',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
